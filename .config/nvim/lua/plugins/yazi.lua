c3776cd427fb3e221b4197a5dd0e00395874bd9e
whoislikemiha <mihabubnjevic@gmail.com>
2025-11-03 12:05:22 +0100

    tmux config and theme

--- .config/nvim/lua/plugins/yazi.lua
+++ .config/nvim/lua/plugins/yazi.lua
@@ -1,16 +1,12 @@
 return {
+  {
     "DreamMaoMao/yazi.nvim",
     dependencies = {
-        "nvim-telescope/telescope.nvim",
-        "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
+      "nvim-lua/plenary.nvim",
     },
     keys = {
-        { "<leader>y", "<cmd>Yazi<CR>", desc = "Toggle Yazi" },
+      { "<leader>y", "<cmd>Yazi<CR>", desc = "Toggle Yazi" },
     },
-    -- Optional: Configure yazi.nvim if needed
-    -- config = function()
-    --   require("yazi").setup({
-    --     -- Your Yazi configuration options here
-    --   })
-    -- end,
+  },
 }
